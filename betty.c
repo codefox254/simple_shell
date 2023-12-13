b50300459d9455290c4c2490e56c45c27dcdd436
codefox254 <frankode254@gmail.com>
2023-12-12 21:39:11 -0800

    pushing first files

---
+++ betty.c
@@ -0,0 +1,24 @@
+/*
+ * File: beautiful_code.c
+ * Author: @odero_francis and @hellen_maseko
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("Hello, Betty! %d\n", i);
+    }
+
+    return 0;
+}
+
